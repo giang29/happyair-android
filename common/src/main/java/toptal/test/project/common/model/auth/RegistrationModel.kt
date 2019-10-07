052681b03b6e78395558c90c2254ca5ef4ad3686
Giang Pham <giangp@metropolia.fi>
2019-10-07 22:46:14 +0300

    Remove unnecessary files

--- common/src/main/java/toptal/test/project/common/model/auth/RegistrationModel.kt
+++
@@ -1,7 +0,0 @@
-package toptal.test.project.common.model.auth
-
-data class RegistrationModel(
-    val username: String,
-    val email: String,
-    val password: String
-)
--- common/src/main/java/toptal/test/project/common/model/auth/SettingsModel.kt
+++
@@ -1,5 +0,0 @@
-package toptal.test.project.common.model.auth
-
-data class SettingsModel(
-    val expectedIntakeCaloriesPerDay: Int
-)
