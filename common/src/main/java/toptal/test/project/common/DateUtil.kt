57e78685dcb122bb84a0c729f7ac044311b2856c
Giang Pham <giangp@metropolia.fi>
2019-10-23 20:43:00 +0300

    Simplify code

---
+++ common/src/main/java/toptal/test/project/common/DateUtil.kt
@@ -0,0 +1,7 @@
+package toptal.test.project.common
+
+import java.text.SimpleDateFormat
+
+val fullDateFormat = SimpleDateFormat("MMM d yy")
+val shortDateFormat = SimpleDateFormat("MMM yy")
+val weekFormat = SimpleDateFormat("w yy")
