71211ef86f9597d10a04396645688b56057e2a16
Giang Pham <giangp@metropolia.fi>
2019-10-31 21:33:26 +0200

    Disallow user to spam a tab

--- app/src/main/java/toptal/test/project/meal/home/HomeFragment.kt
+++ app/src/main/java/toptal/test/project/meal/home/HomeFragment.kt
@@ -2,8 +2,9 @@ package toptal.test.project.meal.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.forEach
 import androidx.navigation.Navigation.findNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.NavigationUI.onNavDestinationSelected
 import kotlinx.android.synthetic.main.f_home.*
 import toptal.test.project.meal.R
 import toptal.test.project.meal.base.StatelessBaseFragment
@@ -14,6 +15,17 @@ internal class HomeFragment : StatelessBaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        f_home_navigation.setupWithNavController(findNavController(requireActivity(), R.id.f_home_nav_host))
+        f_home_navigation.menu.forEach {
+            it.isEnabled = !it.isChecked
+        }
+        f_home_navigation.setOnNavigationItemSelectedListener { item ->
+            f_home_navigation.menu.forEach {
+                it.isEnabled = item.itemId != it.itemId
+            }
+            onNavDestinationSelected(
+                item,
+                findNavController(requireActivity(), R.id.f_home_nav_host)
+            )
+        }
     }
 }
