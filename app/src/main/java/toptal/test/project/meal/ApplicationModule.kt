cc5b753d718c1b0f50fed4cc74ff4895838b9cad
Giang Pham <giangp@metropolia.fi>
2019-11-29 10:44:32 +0200

    Remove redundant code

--- app/src/main/java/toptal/test/project/meal/ApplicationModule.kt
+++ app/src/main/java/toptal/test/project/meal/ApplicationModule.kt
@@ -4,10 +4,8 @@ import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import org.kodein.di.Kodein
 import org.kodein.di.generic.*
-import toptal.test.project.common.SharedPreferencesHelper
 import toptal.test.project.common.TAG_SCHEDULER_POST_EXECTION
 import toptal.test.project.common.TAG_STRING_BASE_URL
-import toptal.test.project.meal.common.SharedPreferencesHelperImpl
 
 
 val applicationModule: Kodein.Module = Kodein.Module("ApplicationModule") {
@@ -15,10 +13,5 @@ val applicationModule: Kodein.Module = Kodein.Module("ApplicationModule") {
     bind<Scheduler>(tag = TAG_SCHEDULER_POST_EXECTION) with provider {
         AndroidSchedulers.mainThread()
     }
-
-    bind<SharedPreferencesHelper>() with singleton {
-        SharedPreferencesHelperImpl(instance())
-    }
-
     constant(tag = TAG_STRING_BASE_URL) with "https://meal-management-api.herokuapp.com/"
 }
